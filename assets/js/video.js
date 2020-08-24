e7ed5374067b56b622e7ac503aecf9e795608b0d
theoleanse <theoleanse@gmail.com>
2020-08-24 19:45:44 +0100

    slugs, protocol, custom ecto type, beginning sockets

---
+++ assets/js/video.js
@@ -0,0 +1,20 @@
+import Player from "./player.js";
+let Video = {
+  init(socket, element) {
+    if (!element) return;
+    let playerId = element.getAttribute("data-player-id");
+    let videoId = element.getAttribute("data-id");
+    socket.connect();
+    Player.init(element.id, playerId, () => {
+      this.onReady(videoId, socket);
+    });
+  },
+  onReady(videoId, socket) {
+    let msgContainer = document.getElementById("msg-container");
+    let msgInput = document.getElementById("msg-input");
+    let postButton = document.getElementById("msg-submit");
+    let vidChannel = socket.channel("videos:" + videoId);
+    // TODO join the vidChannel
+  }
+};
+export default Video;
